248362d09155f383bc9ffd949f8984897d2c4a99
Seu Nome <seu-email@example.com>
2024-11-17 13:30:33 -0300

    first commit

--- app.py
+++ app.py
@@ -138,5 +138,5 @@ def upload():
 
 if __name__ == '__main__':
     threading.Thread(target=scheduler_thread, daemon=True).start()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 5000))  # Heroku atribui a porta automaticamente
+    app.run(host='0.0.0.0', port=port, debug=False)  # Mude debug para False em produção
